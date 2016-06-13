8d36ca15eaf1b18625fa4a838367ec521fa320ae
Julien Ammous <schmurfy@gmail.com>
2016-06-13 16:42:11 +0200

    improve test example

--- examples/test.rb
+++ examples/test.rb
@@ -1,6 +1,7 @@
 
 
-module EchoHandler #< Mongoose::ConnectionHandler
+# the module will be mixed into the connection object
+module EchoHandler
   def accepted()
     puts "new connection from #{remote_address} to #{local_address}, hello !"
   end
@@ -9,18 +10,23 @@ module EchoHandler #< Mongoose::ConnectionHandler
     puts "good bye :("
   end
   
-  def data_received(data)
-    puts "received #{data.inspect}, send it back"
-    send_data(data)
-    close()
+  def timer
+    send_data "timer triggered !"
   end
   
-  
-  # def data_queued(c)
-  # end
-  
-  # def polled()
-  # end
+  def data_received(data)
+    case data[0]
+    when '$' then data = data[1..-1]; close_after_send()
+    when '%' then data = data[1..-1]; close()
+    when 'd'
+      t = data.split(" ")[1].to_i
+      set_timer(t)
+      return
+    end
+    
+    puts "received #{data.inspect}, send it back"
+    send_data(data)
+  end
   
 end
 
@@ -29,7 +35,10 @@ manager = Mongoose::Manager.new
 # the module will be mixed into the connection object
 # manager.bind('tcp://1234', EchoHandler)
 
-manager.bind('tcp://1234', EchoHandler)
+# ipv6
+# manager.bind('tcp://[::1]:1234', EchoHandler)
 
+# ipv4
+manager.bind('tcp://127.0.0.1:1234', EchoHandler)
 
 manager.run()
