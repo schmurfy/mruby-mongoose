44435c4ee68a03074e743a1fb2f307b95c0eaba9
Julien Ammous <schmurfy@gmail.com>
2016-06-15 16:42:44 +0200

    adds example lines to enable logging

--- src/gem.c
+++ src/gem.c
@@ -9,6 +9,9 @@ void mrb_mruby_mongoose_gem_init(mrb_state *mrb)
   struct RClass *mod = mrb_define_module(mrb, "Mongoose");
   int ai = mrb_gc_arena_save(mrb);
   
+  // cs_log_set_level(LL_VERBOSE_DEBUG);
+  // cs_log_set_file(stdout);
+  
   gem_init_connection_class(mrb, mod);
   gem_init_manager_class(mrb, mod);
   mrb_gc_arena_restore(mrb, ai);
