4d2392d68619d6c274e2d84faf5ce77ccf4b955f
Julien Ammous <schmurfy@gmail.com>
2016-06-27 13:00:25 +0200

    renames mongoose_authentication_data

--- include/mg_struct.h
+++ include/mg_struct.h
@@ -5,16 +5,16 @@ typedef struct {
   char *user;
   char *pass;
   char *type;
-} authentication_data;
+} mongoose_authentication_data;
 
 typedef struct {
-  struct mg_connection  *conn;
-  struct RClass         *m_class;
-  mrb_state             *mrb;
-  mrb_value             m_handler;
-  mrb_value             m_arg;
-  uint8_t               protocol;
-  authentication_data   auth;
+  struct mg_connection            *conn;
+  struct RClass                   *m_class;
+  mrb_state                       *mrb;
+  mrb_value                       m_handler;
+  mrb_value                       m_arg;
+  uint8_t                         protocol;
+  mongoose_authentication_data    auth;
 } mongoose_connection_state;
 
 #endif // _MG_STRUCT_H
